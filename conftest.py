435d29b546d56f88397322d21b38c3d5597481ce
Build Agent <build@coralnpu.local>
2026-03-22 06:07:29 +0000

    fix(pytest): eradicate global timeout monkeypatch on Popen.wait() to prevent xdist worker crashes

--- conftest.py
+++ conftest.py
@@ -1,46 +1,10 @@
 import atexit
 import os
 import signal
-import subprocess
 
 os.environ["DISABLE_COMPILER_CACHE"] = "1"
 
 active_pids = set()
-
-_original_popen = subprocess.Popen
-
-class _TrackedPopen(_original_popen):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        active_pids.add(self.pid)
-
-    def wait(self, *args, **kwargs):
-        try:
-            ret = super().wait(*args, **kwargs)
-        except subprocess.TimeoutExpired:
-            try: os.kill(self.pid, signal.SIGKILL)
-            except OSError: pass
-            raise
-        active_pids.discard(self.pid)
-        return ret
-
-    def poll(self):
-        ret = super().poll()
-        if ret is not None:
-            active_pids.discard(self.pid)
-        return ret
-
-    def communicate(self, *args, **kwargs):
-        try:
-            ret = super().communicate(*args, **kwargs)
-        except subprocess.TimeoutExpired:
-            try: os.kill(self.pid, signal.SIGKILL)
-            except OSError: pass
-            raise
-        active_pids.discard(self.pid)
-        return ret
-
-subprocess.Popen = _TrackedPopen
 
 def teardown_worker_group():
     for pid in list(active_pids):
