f58a382a1661e425b94c86de20a82f470771b481
Build Agent <build@coralnpu.local>
2026-04-11 06:02:05 +0000

    test(coralnpu): fix API deprecation and exception swallowing in prototype suites
    Resolves detonating stub failures caused by deprecated `Device.DEFAULT` usage by replacing it with `DEV.value`. Additionally removes `except Exception: pass` wrappers that swallowed `OutOfMemoryError` bounds, replacing them with explicit `self.fail(\"Not Implemented\")` to serve as authentic prototype milestones.

--- test/test_coralnpu_suites.py
+++ test/test_coralnpu_suites.py
@@ -6,43 +6,37 @@ class Int8QuantizationValidationSuite(unittest.TestCase):
     @pytest.mark.prototype
     def test_int8_quantization_bounds(self):
         from tinygrad import Tensor, dtypes, Device
+        from tinygrad.helpers import DEV
         import os
         os.environ["DEV"] = "CORALNPU"
-        Device.DEFAULT = "CORALNPU"
+        DEV.value = "CORALNPU"
         a = Tensor.ones(10, dtype=dtypes.int8)
         b = Tensor.ones(10, dtype=dtypes.int8)
-        try:
-            (a + b).realize()
-        except Exception:
-            pass
+        self.fail("Not Implemented")
 
 @pytest.mark.prototype
 class BareMetalPingPongDmaSuite(unittest.TestCase):
     @pytest.mark.prototype
     def test_baremetal_pingpong_dma(self):
         from tinygrad import Tensor, Device
+        from tinygrad.helpers import DEV
         import os
         os.environ["DEV"] = "CORALNPU"
-        Device.DEFAULT = "CORALNPU"
+        DEV.value = "CORALNPU"
         a = Tensor.ones(256, 256)
-        try:
-            (a + 1).realize()
-        except Exception:
-            pass
+        self.fail("Not Implemented")
 
 @pytest.mark.prototype
 class OpsCoralNpuParallelSuite(unittest.TestCase):
     @pytest.mark.prototype
     def test_ops_coralnpu_parallel(self):
         from tinygrad import Tensor, Device
+        from tinygrad.helpers import DEV
         import os
         os.environ["DEV"] = "CORALNPU"
-        Device.DEFAULT = "CORALNPU"
+        DEV.value = "CORALNPU"
         a = Tensor.ones(10)
-        try:
-            (a + 1).realize()
-        except Exception:
-            pass
+        self.fail("Not Implemented")
 
 if __name__ == '__main__':
     unittest.main()
